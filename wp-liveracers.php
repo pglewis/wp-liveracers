f9af258e711f76be8d75fcffefd156f0309e10f4
pglewis <pglewis13@gmail.com>
2014-06-03 00:13:17 -0400

    license file, copyright and license notices

--- wp-liveracers.php
+++ wp-liveracers.php
@@ -5,6 +5,24 @@ Plugin URI: https://github.com/pglewis/wp-liveracers
 Description: Support for the LiveRacers "Live Widget" in WordPress
 Version: 1.0
 Author: Phil Lewis
+Text Domain: wp-liveracers
+
+Copyright 2014 Phil Lewis
+
+This file is part of wp-liveracers
+
+wp-liveracers is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /**
